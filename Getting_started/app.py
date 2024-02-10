f3dfafd4aeecc9107b3eb85d4efdad92691e6d51
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-02-10 00:09:21 -0800

    caching

--- Getting_started/app.py
+++ Getting_started/app.py
@@ -1,13 +1,27 @@
-# Flask for Hello World 
-
 from flask import Flask
 import os
+import time 
+import redis
 
 app = Flask(__name__)
+cache = redis.Redis(host = 'redis', port=6379)
 
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            cache.reset_retry_count()
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exec:
+            if redis ==0:
+                raise exec
+            retries -= 1
+            time.sleep(0.5)
+            
 @app.route('/', methods=['GET'])
 def home():
-    return "Hello World"
+    count = get_hit_count()
+    return "Hello Krushna! I have been seen {} times\n".format(count)
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=8000)
